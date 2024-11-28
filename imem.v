99e426dcc31de55e29828d8958b505fa7b09142e
m41k1 <michalhinojosa@gmail.com>
2024-11-28 17:43:51 -0500

    final

--- imem.v
+++ imem.v
@@ -5,6 +5,9 @@ module imem (
 	input wire [31:0] a;
 	output wire [31:0] rd;
 	reg [31:0] RAM [63:0];
+	assign RAM[0[31:2]] = 8'he04f000f;
+
+	
 	initial $readmemh("memfile_example.mem", RAM);
 	assign rd = RAM[a[31:2]];
 endmodule
