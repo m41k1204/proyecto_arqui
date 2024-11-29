c8a902d4608efa8272fc8561904616982fc0a859
m41k1204 <155772773+m41k1204@users.noreply.github.com>
2024-11-29 18:35:25 -0500

    Update imem.v

--- imem.v
+++ imem.v
@@ -6,6 +6,14 @@ module imem (
 	output wire [31:0] rd;
 	reg [31:0] RAM [63:0];
 	
-	initial $readmemh("memfile_example.mem", RAM);
+	initial begin 
+	   RAM[0] = 32'he04f000f;
+	   RAM[1] = 32'he2801004;
+	   RAM[2] = 32'he2802006;
+	   RAM[3] = 32'he2803006;
+	   RAM[4] = 32'he0814003;
+	   RAM[5] = 32'he0425001;
+	   end
+	   
 	assign rd = RAM[a[31:2]];
 endmodule
